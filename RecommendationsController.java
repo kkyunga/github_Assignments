11d93ec3cb952f639236c20cb924bd4e47934f1c
KIM kyung a <119964637+kkyunga@users.noreply.github.com>
2023-04-12 17:11:45 +0900

    Update RecommendationsController.java

--- RecommendationsController.java
+++ RecommendationsController.java
@@ -1,12 +1,1 @@
-package Bare_Minimum.Bare_Minimum;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class RecommendationsController {
-    @GetMapping("/")
-    public String helloWorld() {
-        return "To-do Application ! ";
-    }
-}
